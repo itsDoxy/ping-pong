338e426df9e0f31ddcf5eb8f03baf6d7eea3b103
Pedro Computador <62022785+itsDoxy@users.noreply.github.com>
2023-08-03 21:11:00 -0300

    Ping-Pong version 2.0
    last update on the game using canvas

--- script.js
+++ script.js
@@ -25,15 +25,15 @@ const line = {
     // A largura quero como o próprio campo;
     canvasCtx.fillRect(field.w / 2 - this.w / 2, 0, this.w, field.h);
   }
-
 }
+
 const leftPaddle = {
   x: gapX,
   y: 0,
   w: line.w,
   h: 200,
-  _move: function(){
-    this.y = mouse.y - this.h / 2 ;
+  _move: function(){ 
+    this.y = mouse.y - this.h / 2
   },
   draw: function() {
     canvasCtx.fillStyle = ("#FFFFFF");
@@ -47,9 +47,16 @@ const rightPaddle = {
   y: 100,
   w: line.w,
   h: 200,
+  speed: 2,
   _move: function(){
-    this.y = ball.y
-
+    if (this.y + this.h / 2 < ball.y + ball.r) {
+      this.y += this.speed
+    } else {
+      this.y -= this.speed
+    }
+  },
+  _speedUp: function() {
+    this.speed += 0.7
   },
   draw: function() {
     canvasCtx.fillStyle = ("#FFFFFF");
@@ -64,30 +71,62 @@ const rightPaddle = {
 }
 
 const score = {
-  human: 'Player: ',
-  humanS: 2,
-  computer: 'Game: ',
-  computerS: 1,
+  human: 0,
+  computer: 0,
+  increasePlayer: function() {
+    this.human++
+  },
+  increaseComputer: function() {
+    this.computer++
+  },
   draw: function(){
       canvasCtx.font = "bold 72px Arial";
       canvasCtx.textAlign = "center";
       canvasCtx.textBaseline = "top";
       canvasCtx.fillStyle = '#01341D'
-      canvasCtx.fillText(this.human + this.humanS, field.w / 4, 50) //Left
-      canvasCtx.fillText(this.computer + this.computerS, field.w / 4 + field.w / 2 , 50) //Right
+      canvasCtx.fillText(this.human, field.w / 4, 50) //Left
+      canvasCtx.fillText(this.computer, field.w / 4 + field.w / 2 , 50) //Right
   }
 }
 
 const ball = {
-    x: 370,
-    y: 120,
+    x: 50,
+    y: 0,
     r: 20,
     speed: 3,
     directionX: 1,
     directionY: 1,
     _calcPosition: function() {
+      // verificando se o jogador 1 pontuou (Dado que x > largura do campo)
+      if (this.x > field.w - this.r - rightPaddle.w - gapX){
+          // check if the right paddle is in the position y of the ball
+          if(this.y + this.r > rightPaddle.y &&
+            this.y - this.r < rightPaddle.y + rightPaddle.h ) {
+              // Rebate a bola invertendo o sinal do x
+              this._reverseX()
+          } else {
+            // Score Player 1 aka Human
+            score.increasePlayer()
+            this._pointUp()
+          }
+      }
+      // verificando se o jogador 2 pontuou (Dado que x < 0)
+      if (this.x < this.r + leftPaddle.w + gapX) {
+          // verificando se a raquete da esquerda está na posição y da bola
+          if (this.y + this.r > leftPaddle.y &&
+            this.y - this.r < leftPaddle.y + leftPaddle.h) {
+              // Rebate a bola invertendo o sinal do x
+              this._reverseX() 
+            }else {
+              // Score Player 2 aka Computer
+              score.increaseComputer()
+              this._pointUp()
+            }
+      }
+
       if (
-          (this.y < 0 && this.directionY < 0) || 
+          //Check both upper and lower coners from its field 
+          (this.y - this.r < 0   && this.directionY < 0) || 
           (this.y > field.h - this.r && this.directionY > 0)
         ) {
         this._reverseY()}
@@ -98,11 +137,21 @@ const ball = {
     _reverseY: function() {
       this.directionY *= -1
     },
+    _speedUp: function() {
+      this.speed += 1
+    },
+    _pointUp: function() {
+      this.x = field.w / 2
+      this.y = field.h / 2
+      
+      rightPaddle._speedUp()
+      this._speedUp()
+    },
     _move: function() {
       this.x += this.directionX * this.speed
       this.y += this.directionY * this.speed
     },
-    draw: function(){
+    draw: function() {
       canvasCtx.fillStyle = ("#FFFFFF");
       canvasCtx.beginPath()
       canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false)
